8b67d21311d2ba8d2c0931cb2522d42fbdd468f6
Robert Grumbine <Robert.Grumbine@noaa.gov>
2021-09-24 19:22:32 +0000

    language standard checking

--- sorc/makeall.mk
+++ sorc/makeall.mk
@@ -12,7 +12,7 @@ SHELL=/bin/sh
 #------------------- should need no changes below here ------------------------
 #Compilers and their options
 FC=ftn
-FOPTS=-c -O2 -I $(MMAB_INC)
+FOPTS=-c -O2 -I $(MMAB_INC) -e03
 #FOPTS=-c -O2 $(MMAB_INC) $(NETCDF_INCLUDE)
 
 ##Home desk:
@@ -24,11 +24,17 @@ FLD=$(FC)
 FLDFLAGS=$(MMAB_LIBF4)
 
 CC=cc
+#gnu:
 COPTS=-c -ansi -O2 -DLINUX -I $(MMAB_INC) $(NETCDF_INCLUDE)
+#intel:
+COPTS=-c -O2 -DLINUX -I $(MMAB_INC) $(NETCDF_INCLUDE) -std=c11
 
 CPP=CC
 CPPLD=CC
+#gnu:
 CPPOPTS= -c -ansi -Wall -O2 -DLINUX -DCPLUS -I $(MMAB_INC) $(NETCDF_INCLUDE)
+#intel:
+CPPOPTS= -c -Wall -O2 -DLINUX -DCPLUS -I $(MMAB_INC) $(NETCDF_INCLUDE) -std=c++11
 CPPLDFLAGS=$(MMAB_LIBF4)
 
 #Building elements

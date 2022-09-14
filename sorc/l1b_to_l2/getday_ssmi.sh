bbfde2b800ae99e17d8e52e7d7e08b8532e5bb1f
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-09-14 17:08:19 +0000

    last changes from April 2022 operations implementation

--- sorc/l1b_to_l2/getday_ssmi.sh
+++ sorc/l1b_to_l2/getday_ssmi.sh
@@ -9,15 +9,10 @@ PM=${PM:-12}
 EXDIR=${EXDIR:-.}
 OUTDIR=${OUTDIR:-/u/Robert.Grumbine/noscrub/l2}
 
-#module load EnvVars/1.0.3 ips/19.0.5.281 impi/19.0.5
-#module load bufr/11.3.1 NetCDF/4.5.0 w3nco/2.2.0
-#module load dumpjb/5.1.0 bufr_dumplist/2.3.0
+#Module load EnvVars/1.0.3 ips/19.0.5.281 impi/19.0.5
+#Module load bufr/11.3.1 NetCDF/4.5.0 w3nco/2.2.0
+#Module load dumpjb/5.1.0 bufr_dumplist/2.3.0
 #env | grep -i dcom > ~/dcoms
-
-if [ $PDY -ge 20210809 ] ;
-  echo F15 retired 20210809
-  exit
-fi
 
 set -x
 # Run if we don't already have:

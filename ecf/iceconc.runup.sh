2779b8fe3b279a76c771e25f2214443c67c78ee5
Robert Grumbine <Robert.Grumbine@noaa.gov>
2024-09-27 08:59:02 -0400

    Develop (#28)
    * start to cactus
    
    * cleaner build process for NCO
    
    * incorporate current GEMPAK job
    
    * from ops v4.4.2 Oct 5, 2021
    
    * reruns for NG-GODAS to near real time L2 ice
    
    * v4.5.1 (#21)
    
    * Update README.md
    
    * implementation notes from earlier versions
    
    * bring in NCO changes for wcoss2
    
    * bringing in changes from April 2022 implementation
    
    * files changed by operational implementation April 2022
    
    * last changes from April 2022 operations implementation
    
    ---------
    
    Co-authored-by: Robert Grumbine <grumbinespam@aol.com>
    
    * Develop (#23)
    
    * Update README.md
    
    * implementation notes from earlier versions
    
    * almost clean build w. updated systems and structure
    
    * removing great lakes and moving towards clean run as dev
    
    * fixes issue for getting and processing bufr
    
    * clean build of all wrt language standards
    
    * semiclean working run on cactus
    
    * start towards cmake, remove filter legacy
    
    * update to run up across a span of dates
    
    * early Jan 2024 running
    
    * from system convergences
    
    * starting on cmake builds
    
    * cmake of amsr2 -- execs that need w3, bufr
    
    * cleanup
    
    * mystery problem in run
    
    * mystery was missing skip_hr file
    
    * clean and prep towards implementation
    
    * build without w3nco
    
    * run with most recent versions of modules
    
    * last prep for algorithm updates
    
    ---------
    
    Co-authored-by: Robert Grumbine <grumbinespam@aol.com>
    
    * Delete sorc/general/seaice_grib2.fd/CMakeLists.txt (#34)
    
    * Update makefile (#35)
    
    * Update makefile (#36)
    
    ---------
    
    Co-authored-by: Robert Grumbine <grumbinespam@aol.com>

--- ecf/iceconc.runup.sh
+++ ecf/iceconc.runup.sh
@@ -1,93 +1,48 @@
-#!/bin/bash --login
+#!/bin/bash 
 #####
-#BSUB -J aice_2021
-#BSUB -q "dev"
-#BSUB -P RTO-T2O
-#BSUB -W 2:59
-# #BSUB -W 0:09
-#BSUB -o aice.%J
-#BSUB -e aice.%J
-#BSUB -R "affinity[core(1)]"
-#  #BSUB -R "rusage[mem=1024]"
+#PBS -l select=1:ncpus=1
+#PBS -l walltime=2:59:00
+#PBS -N aice_2024
+#PBS -q "dev"
+#PBS -j oe
+#PBS -A ICE-DEV
+#  #PBS -R "rusage[mem=1024]"
 #####
 
 
 #-----------------------------------------------------------------------------
-echo zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz loading modules zzzzzzzzzzzzzzzzzzzzzzz
-module purge
-module load EnvVars/1.0.3
-module load ips/19.0.5.281 impi/19.0.5
-module load prod_envir/1.1.0
-module load prod_util/1.1.5
-module load grib_util/1.1.1
-module load bufr_dumplist/2.3.0
-module load dumpjb/5.1.0
-module load imagemagick/6.9.9-25
-module load lsf/10.1 #for internal job management, i.e., bkill
-module list
-echo zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz done loading modules zzzzzzzzzzzzzzzzzz
-
-# Bring the various environment-sensitive definitions out of J jobs and to here:
-#NCO refers to these as 'job card' variables
 set -x
 
-tagm=20210408
-tag=20210409
-end=20210409
+export tagm=20240315
+export tag=20240316
+export end=20240318
 
-export HOMEbase=/u/Robert.Grumbine/rgdev
+echo zzz $tagm
+
+export HOMEbase=/u/robert.grumbine/rgdev
 export seaice_analysis_ver=v4.4.0
 export HOMEseaice_analysis=$HOMEbase/seaice_analysis.${seaice_analysis_ver}
 
 #Use this to override system in favor of my archive:
-export DCOMROOT=/u/Robert.Grumbine/noscrub/satellites/
-export RGTAG=prod
-export my_archive=true
+#export DCOMROOT=/u/robert.grumbine/noscrub/satellites/
+#export RGTAG=prod
+#export my_archive=true
 
 cd $HOMEseaice_analysis/ecf
 
-export COMINsst_base=/u/Robert.Grumbine/noscrub/sst/prod/sst
-. ./jobcards
+#export COMINsst_base=/u/robert.grumbine/noscrub/sst/prod/sst
 
-if [ -z $obsproc_dump_ver ] ; then
-  echo null obsproc_dump_ver
-  export obsproc_dump_ver=v4.0.0
-  export obsproc_shared_bufr_dumplist_ver=v1.4.0
-fi
-
-########################################################
-#DBN stuff -- now in jobcards
-########################################################
 
 #--------------------------------------------------------------------------------------
 #The actual running of stuff
 
 while [ $tag -le $end ]
 do
-  export PDY=$tag
-  export PDYm1=$tagm
 
-  if [ $my_archive == "true" ] ; then
-    export DCOM=${DCOMROOT}/$RGTAG/$PDY
-  fi
+  time ./day.sh
 
-  export job=seaice_filter
-  export DATA=$DATAROOT/${job}.${pid}
-  #script handles make: mkdir $DATA
-  time ./sms.filter.fake > /u/Robert.Grumbine/noscrub/com/sms.filter.$tag
-
-  export job=seaice_analysis
-  export DATA=$DATAROOT/${job}.${pid}
-  #script handles make: mkdir $DATA
-#Required for dumpjb to run:
-  export TMPDIR=$DATA
-  time ./sms.fake > /u/Robert.Grumbine/noscrub/com/sms.$tag
-
-#  module load gempak
-#  time ../jobs/JICE_GEMPAK > gempak.$tag
-
-  tagm=$tag
+  export tagm=$tag
   tag=`expr $tag + 1`
-  tag=`dtgfix3 $tag`
+  export tag=`$HOME/bin/dtgfix3 $tag`
 
 done

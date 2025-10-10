9347f94aa9b9d124e1142c834c4a2e55d14207a4
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2025-10-10 13:57:24 +0000

    lightly tested viirs compositing script

--- scripts/exseaice_viirs.sh
+++ scripts/exseaice_viirs.sh
@@ -15,34 +15,36 @@ echo zzzzzzzzzzzzzzz  exseaice_viirs: DCOMROOT = $DCOMROOT
 
 if [ $cyc == '00' ] ; then
   day=$PDYm1
-  hh='18 19 20 21 22 23'
+  hours='18 19 20 21 22 23'
 else
   day=$PDY
   if [ $cyc == '06' ] ; then
-    hh='00 01 02 03 04 05'
+    hours='00 01 02 03 04 05'
   fi
   if [ $cyc == '12' ] ; then
-    hh='06 07 08 09 10 11'
+    hours='06 07 08 09 10 11'
   fi
   if [ $cyc == '18' ] ; then
-    hh='12 13 14 15 16 17'
+    hours='12 13 14 15 16 17'
   fi
 fi
 
 echo zzz $day $hh
-exit 0
 
 # How to manage hh?  RG
 echo zzzzz working on viirs $PDY $cyc
 for inst in j01 npp n21
 do
-  python3 composite.py \
-  $DCOMROOT/$PDY/viirs/JRR-IceConcentration*_${inst}_s${day}${hh}*.nc \
-  > output.$inst.$cyc.$day 
-  if [ ! -f output.$inst.$cyc.$day ] ; then
-    touch output.$inst.$cyc.$day
-  fi
+  for hh in $hours
+  do
+    python3 composite.py \
+    $DCOMROOT/$PDY/viirs/JRR-IceConcentration*_${inst}_s${day}${hh}*.nc \
+    > output.$inst.$cyc.${day}$hh 
+    if [ ! -f output.$inst.$cyc.${day}$hh ] ; then
+      touch output.$inst.$cyc.${day}$hh
+    fi
+  done
 done
 # Handle no file case RG
-mv output.*.$cyc.$day $COMOUT
+mv output.*.$cyc.${day}?? $COMOUT
 

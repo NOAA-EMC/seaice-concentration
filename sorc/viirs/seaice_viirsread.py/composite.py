daeac35d817048fde05ee269467d6ceeeaeffd20
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2025-10-10 13:59:53 +0000

    ascii output from initial compositor

--- sorc/viirs/seaice_viirsread.py/composite.py
+++ sorc/viirs/seaice_viirsread.py/composite.py
@@ -95,12 +95,18 @@ for k in range(0,len(indices[0])):
     tsumx[j,i] /= gcount[j,i]
     tsumx2[j,i] = sqrt(max(0., tsumx2[j,i]/gcount[j,i] - tsumx[j,i]*tsumx[j,i]) )
     target_grid.locate(i,j,z)
-    #debug: print(i,j,z.lat, z.lon, csumx[j,i], csumx2[j,i], tsumx[j,i], tsumx2[j,i], gcount[j,i], flush=True, file=sys.stdout)
+   
+    print(i,j,z.lat, z.lon, csumx[j,i], csumx2[j,i], tsumx[j,i], tsumx2[j,i], gcount[j,i], flush=True, file=sys.stdout)
     cellcount += 1
 
 print("gcount, avg: ",gcount.max(), gcount.min(), tsumx.max(), tsumx.min(), tsumx2.max(), tsumx2.min(),file=sys.stderr  )
 print("cellcount = ",cellcount,file=sys.stderr)
 
+exit(0)
+
+#----------------------------------------------------------------------------------
+# Here and below is pre-adaptation for netcdf output to be used downstream
+# It is fully functional, but not polished
 #----------------------------------------------------------------------------------
 
 class ncout:

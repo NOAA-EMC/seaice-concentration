8d140aca57ffe8c34deb20b4bdb766c7f80dc8ed
Robert Grumbine <Robert.Grumbine@noaa.gov>
2025-11-24 09:43:04 -0500

    Develop (#54)
    * ecf out is a mistake, otherwise, dropping back to unify ops and rgrumbine repos
    
    * remove accidental out files
    
    * remove developer-only files
    
    * removing developer-only files

--- ecf/iceconc.runup.sh
+++ ecf/iceconc.runup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 #####
 #PBS -l select=1:ncpus=1
-#PBS -l walltime=2:59:00
-#PBS -N aice_2024
+#PBS -l walltime=9:59:00
+#PBS -N aice8
 #PBS -q "dev"
 #PBS -j oe
 #PBS -A ICE-DEV
@@ -13,18 +13,14 @@
 set -x
 
 export NRT=NO
-export tagm=20250928
-export tag=20250929
-export end=20251014
+export tagm=20250725
+export tag=20250726
+export end=20250831
 
 export HOMEbase=/u/robert.grumbine/rgdev
 export seaice_analysis_ver=v4.5.2
 
-echo zzz tagm = $tagm
-
 export HOMEseaice_analysis=$HOMEbase/seaice_analysis.${seaice_analysis_ver}
-
-echo zzz tagm = $tagm
 
 #Use this to override system in favor of my archive:
 if [ $NRT == 'NO' ] ; then
